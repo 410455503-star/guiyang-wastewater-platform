73ed7e2ed0d301540782dd0abd98ab3770612f49
410455503-star <410455503@qq.com>
2026-06-30 06:13:00 +0800

    Wrap App component with HashRouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+// 引入HashRouter哈希路由
+import { HashRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    {/* 外层包裹哈希路由 */}
+    <HashRouter>
+      <App />
+    </HashRouter>
   </StrictMode>,
 )
